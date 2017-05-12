918de4c8436a9857a7f0c317647d4b72ab60ada9
Jeferson Barros <soldier.jhonson@gmail.com>
2017-05-12 18:58:56 -0400

    Add logo splash

--- js/states/menu_state.js
+++ js/states/menu_state.js
@@ -6,6 +6,7 @@
 
     MenuState.prototype.preload = function() {
         this.onMenu = true;
+        this.game.load.image('background', 'assets/img/LOGO_SPLASH.png');
         this.game.load.image('start', 'assets/img/start1.png');
         this.game.load.audio('environment', 'assets/audio/environment.ogg');
         this.game.load.audio('clickSound', 'assets/audio/click.ogg');
@@ -13,14 +14,16 @@
     
     MenuState.prototype.create = function() {
         
+        this.game.add.tileSprite(0, 0, 800, 600, 'background');
+        
         this.clickSound = this.game.add.audio('clickSound');
         
         this.onMenu = true;
         
-        var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 150, 'Vampixel', { fill: '#ffffff', align: 'center', fontSize: 80 });
-        text.anchor.set(0.5);
+        //var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 150, 'Vampixel', { fill: '#ffffff', align: 'center', fontSize: 80 });
+        //text.anchor.set(0.5);
 
-        var button = this.game.add.button(this.game.world.centerX, this.game.world.centerY, 'start', clicked, this);
+        var button = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 150, 'start', clicked, this);
         button.anchor.set(0.5);
 
         function clicked() {
